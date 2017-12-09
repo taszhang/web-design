7a41c7d4c64d87bd9c6f5b4bbe5dd7ac337fa13b
Jinzi Zhang <gracezhang@172-16-210-158.DYNAPOOL.NYU.EDU>
2017-12-08 22:35:34 -0500

    first commit

---
+++ external.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("img").hide();
+    });
+});
+$(document).ready(function(){
+    $("p:even").css("border", "1px solid red");
+});
+
+$(document).ready(function(){
+    alert($("#alert").html());
+});
+
+$(document).ready(function(){
+
+})
